400fd505b6bad3826c2113db2640fa0e3765a095
Steven_Nicks <stevenalvradopaez@gmail.com>
2025-09-25 17:02:09 -0500

    dashboard

--- src/controllers/empleado.controller.js
+++ src/controllers/empleado.controller.js
@@ -3,7 +3,8 @@ import EmpleadoModel from '../models/empleadoModel.js'
 export const getAllEmpleados = async (req, res, next) => {
    try {
       const empleados = await EmpleadoModel.getAllEmpleados();
-      res.json(empleados[0]);
+      // res.json(empleados[0]);
+      res.render("dashboard/index", { title: "Empleados", empleados: empleados[0] });
    } catch (err) {
       next(err); // lo manda al errorHandler
    }
