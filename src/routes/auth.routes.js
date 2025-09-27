c8564ec7d836a0119e79325d028bf990dc7ce825
Steven_Nicks <stevenalvradopaez@gmail.com>
2025-09-27 09:44:56 -0500

    fix(routes) se corrigen las rutas de login y register

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { renderLogin, login } from "../controllers/auth.controller.js";
+import { renderLogin, login, renderRegister } from "../controllers/auth.controller.js";
 
 const router = Router();
 
-router.get("/", renderLogin);
+router.get("/login", renderLogin);
+router.get("/register", renderRegister);
 router.post("/", login);
 
 export default router;
