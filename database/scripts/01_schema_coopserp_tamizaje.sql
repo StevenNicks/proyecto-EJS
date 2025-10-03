e659522e77fda852b05c9e9d2d62ae7acd7d1b15
Steven_Nicks <stevenalvradopaez@gmail.com>
2025-10-03 15:42:49 -0500

    se cambia la estructura de la DB

--- database/scripts/01_schema_coopserp_tamizaje.sql
+++ database/scripts/01_schema_coopserp_tamizaje.sql
@@ -25,26 +25,15 @@ CREATE TABLE
 CREATE TABLE
    usuarios (
       id INT AUTO_INCREMENT PRIMARY KEY,
-      nombre VARCHAR(100) NOT NULL,
+      -- nombre VARCHAR(100) NOT NULL,
+      empleado_cedula VARCHAR(20) NOT NULL,
       email VARCHAR(100) NOT NULL UNIQUE,
       password VARCHAR(255) NOT NULL,
       rol_id INT NOT NULL DEFAULT 2,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      FOREIGN KEY (empleado_cedula) REFERENCES empleados (cedula) ON DELETE CASCADE ON UPDATE CASCADE,
       FOREIGN KEY (rol_id) REFERENCES roles (id) ON DELETE CASCADE ON UPDATE CASCADE
-   ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
-
--- Tabla intermedia: relaciona usuarios y empleados
-CREATE TABLE
-   usuario_empleado (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      usuario_id INT NOT NULL,
-      empleado_id INT NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-      FOREIGN KEY (usuario_id) REFERENCES usuarios (id) ON DELETE CASCADE ON UPDATE CASCADE,
-      FOREIGN KEY (empleado_id) REFERENCES empleados (id) ON DELETE CASCADE ON UPDATE CASCADE,
-      UNIQUE KEY usuario_empleado_unique (usuario_id, empleado_id)
    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- Insertar roles básicos
@@ -69,17 +58,12 @@ VALUES
 
 -- Insertar usuarios
 INSERT INTO
-   usuarios (nombre, email, password)
+   usuarios (empleado_cedula, email, password)
 VALUES
    (
-      'steven alvarado paez',
+      '1143994968',
       'stevenalvaradopaez@gmail.com',
       '$2a$12$0oxFNNOLCx1w7IK3SPMTfea7fnYq25/Yg7NU4RfZqQmxX2pydztMy'
    );
 
--- Insertar relacion usuario + empleado + rol
-INSERT INTO
-   usuario_empleado (usuario_id, empleado_id)
-VALUES
-   -- Usuario 1 (Empleado 1) con rol Admin
-   (1, 1);
+-- DROP TABLE nombre_de_la_tabla;
